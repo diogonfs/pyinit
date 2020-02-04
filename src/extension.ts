160843e985f53d4f93453fb1a102b2165e36d6d1
Diogo Nolasco <diogonolascofs@gmail.com>
2020-02-04 19:35:53 -0300

    fix sulfolders recursive generation

--- src/extension.ts
+++ src/extension.ts
@@ -3,41 +3,40 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import { debug } from 'util';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pyinit" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.generateInit', async (fileObj) => {
-		// The code you place here will be executed every time your command is executed
-		let activeEditor = vscode.window.activeTextEditor;
-		let counter = 0
+		
+		let counter = 0;
 		if(!fileObj) {
-			await vscode.commands.executeCommand('copyFilePath')
-			fileObj = await vscode.env.clipboard.readText()
+			await vscode.commands.executeCommand('copyFilePath');
+			fileObj = await vscode.env.clipboard.readText();
 		}
 
 		if (fileObj !== null && fileObj !== undefined) {
-			let dirname = !(typeof fileObj === 'string') ? fileObj.path : fileObj 
-			let dirs = fs.readdirSync(dirname).filter((file) => {
-				return fs.statSync(dirname +'/'+ file).isDirectory();
-			}).map((file) => {
-				return dirname + '/' + file
-			});
-			dirs.push(dirname)
+			let dirname = !(typeof fileObj === 'string') ? fileObj.path : fileObj ;
+			let dirs = [];
 
+			function traverseDir(dirname: any) {
+				fs.readdirSync(dirname).forEach(file => {
+				  	let fullPath = path.join(dirname, file);
+				  	if (fs.lstatSync(fullPath).isDirectory()) {
+						dirs.push(fullPath);
+						traverseDir(fullPath);
+				   }  
+				});
+			}
+
+			traverseDir(dirname)
+			dirs.push(dirname)
+			
 			dirs.forEach((dir_path) => {
 				if (!fs.existsSync(path.join(dir_path, "__init__.py"))) {
-					counter++
-					fs.writeFileSync(path.join(dir_path, '__init__.py'), "")
+					counter++;
+					fs.writeFileSync(path.join(dir_path, '__init__.py'), "");
 				}
 			})
 			
